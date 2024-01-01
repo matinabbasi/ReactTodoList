d05102d925a6271bf0474bf6de6bc1e7caca4260
matin abbasi <matabbasi98@gmail.com>
2024-01-01 15:03:32 +0330

    add try catch

--- src/components/api_call/api_call.jsx
+++ src/components/api_call/api_call.jsx
@@ -1,53 +1,70 @@
-import { React, useState } from "react";
+import { Component } from "react";
 import "./api_call_style.css";
-const ApiCall = () => {
-  const [respons, setResponse] = useState([]);
+import axios from "axios";
 
-  return (
-    <>
-      <div className="row">
-        <div className="text-center">
-          <button
-            onClick={() => {}}
-            className="btn btn-primary text-center"
-          >
-            call Api
-          </button>
-        </div>
-      </div>
-      <div className="api-call text-center m-3 p-3">
-        <div className="">
-          <header className="post-header">
-            <h2>the last moon</h2>
-            <p className="">author : date :</p>
-          </header>
-          <img
-            className="image"
-            src="https://apod.nasa.gov/apod/image/2312/2023_12_27-fullmoon1024.jpg"
-            alt="moon"
-          />
-          <p>
-            Known to some in the northern hemisphere as December's Cold Moon or
-            the Long Night Moon, the last full moon of 2023 is rising in this
-            surreal mountain and skyscape. The Daliesque scene was captured in a
-            single exposure with a camera and long telephoto lens near Monte
-            Grappa, Italy. The full moon is not melting, though. Its stretched
-            and distorted appearance near the horizon is caused as refraction
-            along the line of sight changes and creates shifting images or
-            mirages of the bright lunar disk. The changes in atmospheric
-            refraction correspond to atmospheric layers with sharply different
-            temperatures and densities. Other effects of atmospheric refraction
-            produced by the long sight-line to this full moon rising include the
-            thin red rim seen faintly on the distorted lower edge of the Moon
-            and a thin green rim along the top.
-          </p>
-          <button className="btn btn-primary">HD picture</button>
-        </div>
-      </div>
-    </>
-  );
-};
+class ApiCall extends Component {
+  state = {
+    api: [],
+    error: null,
+  };
 
-// handleCallApi = () => {};
+  fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+      );
+      this.setState({ api: response.data, error: null });
+    } catch (error) {
+      if (error.response && error.response.status === 429) {
+        // Handle rate limit exceeded error
+        console.error("Too Many Requests. Retrying in 30 seconds...");
+        setTimeout(() => {
+          this.fetchData(); // Retry the request after a delay
+        }, 30000); // Retry after 5 seconds
+      } else {
+        // Handle other errors
+        this.setState({ error: "Error fetching data" });
+      }
+    }
+  };
+
+  async componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    const arrayToRender = Array(4).fill(0);
+    const { api, error } = this.state;
+    return (
+      <>
+        {error ? (
+          <div className="error-message">429 status code (Too Many Requests) from the server, it means that you have exceeded the rate limit for making requests</div>
+        ) : (
+          <div className="row">
+            {arrayToRender.map((e, index) => (
+              <div className="api-call text-center m-3 p-3" key={index}>
+                <header className="post-header">
+                  <h6>{api.title}</h6>
+                  <p className="">date : {api.data}</p>
+                </header>
+                <img className="image" src={api.url} alt="moon" />
+                <p>{api.explanation}</p>
+                <button
+                  className="btn btn-primary"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    window.open(api.hdurl, "_blank");
+                  }}
+                >
+                  HD picture
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+      </>
+    );
+  }
+}
 
 export default ApiCall;
